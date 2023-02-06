446cea0c212bfde46f8fe0b8b21d2a18c3f5d91a
danieljappe <112632294+danieljappe@users.noreply.github.com>
2023-02-06 13:31:22 +0000

    done done

---
+++ fix-samples.sh
@@ -0,0 +1,30 @@
+cd samples/animals/countries
+mv movies ./..
+cd ..
+mv movies ./..
+mv countries ./..
+cd ..
+cd movies
+mv Germany.txt ./..
+cd ..
+mv Germany.txt countries
+cd countries
+mv .apes.txt ./..
+cd ..
+mv .apes.txt animals
+cd animals
+mv Monkies.txt Monkeys.txt
+cd ..
+cd movies
+cat Sex.txt And.txt The.txt City.txt > "Sex And The City".txt
+rm Sex.txt And.txt The.txt City.txt
+cd ..
+cd animals
+cat Elephant.txt babies.txt > "Elephant babies".txt
+rm Elephant.txt babies.txt
+cd ..
+cd countries
+cat North.txt Korea.txt > "North Korea".txt
+rm North.txt Korea.txt
+
+
